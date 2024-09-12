ce71cffa70204ddf85b4a4237d22d5bca31cbfbe
BY1502 <sks14502@gmail.com>
2024-09-12 10:32:07 +0900

    제발

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ app.use(
     secret: 'process.env.SECRET_KEY',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      sameSite: 'None',
+      maxAge: 24 * 60 * 60 * 1000,
+    },
   })
 );
 
